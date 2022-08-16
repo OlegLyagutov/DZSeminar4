d86b7c41f418bfbccf8bd71e1044ef0bcd23d977
Oleg <o.lyagutov@gmail.com>
2022-08-16 10:55:41 +0300

    seminar 4 DZ

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.Write("Введите число A: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// double result = Math.Pow(numA, numB);
+// Console.WriteLine($"Ответ: {result}");
+
+// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string sum = Convert.ToString(Number);
+length sum = Number.Length(sum);
+for (int i = 0; i <= Number; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Console.Write("Введите 8 чисел через запятую: ");
+// int[] array = {0, 0, 0, 0, 0, 0, 0, 0}
+// int[] array = new int[8];
