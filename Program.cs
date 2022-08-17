71da11ed97701bff1adb5057ab74aeffd9ae06a3
Oleg <o.lyagutov@gmail.com>
2022-08-17 19:55:10 +0300

    add var3

--- Program.cs
+++ Program.cs
@@ -3,18 +3,19 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 // способ 1
-Console.Write("Введите число A: ");
-int numA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int numB = Convert.ToInt32(Console.ReadLine());
-int Number = 0;
-{
-    for (int i = 1; i <= numB; i++)
-    {
-       Number = numA[i] * numA[i+1];
-    }
-}
-Console.WriteLine($"число А в степени В равно: {Number}");
+// Console.Write("Введите число A: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// int result = numA;
+
+// for (int i = 1; i < numB; i++)
+// {
+//     result = result *numA;
+// }  
+// Console.WriteLine($"число А в степени В равно: {result}");
+
+
 // способ 2
 // Console.Write("Введите число A: ");
 // int numA = Convert.ToInt32(Console.ReadLine());
@@ -25,27 +26,21 @@ Console.WriteLine($"число А в степени В равно: {Number}");
 
 // Задача 27: Напишите программу, которая принимает на 
 // вход число и выдаёт сумму цифр в числе.
-
-Console.Write("Введите число N: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-string sum = Convert.ToString(Number);
-
-for (i = 0;  i < Length; i++)
-{
-    sum = 0;
-    sum = sum += Number[i];
-}
-Console.WriteLine($"Сумма цифр равна {sum}");
-// length sum = Number.Length(sum);
-// for (int i = 0; i <= Number; i++)
-// {
-//     Console.WriteLine(i);
-// }
-
-
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+// Console.Write("Введите число N: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// while (number > 0)
+// {
+//     int result = number % 10;
+//     number /= 10;
+//     sum = sum + result;
+// }
+// Console.WriteLine($"Сумма цифр чисела равна {sum}");
+
 
 // Задача 29: Напишите программу, которая задаёт массив
 // из 8 элементов и выводит их на экран.
@@ -53,6 +48,14 @@ Console.WriteLine($"Сумма цифр равна {sum}");
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-// Console.Write("Введите 8 чисел через запятую: ");
-// int[] array = {0, 0, 0, 0, 0, 0, 0, 0}
-// int[] array = new int[8];
+Console.Write("Введите 8 чисел через запятую: ");
+int[] array = {};
+void PrintArray()
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+         Console.Write(array[i] + " ");
+    }
+}
+Console.WriteLine($"Массив: [ {String.Join(";"), PrintArray()} ]");
